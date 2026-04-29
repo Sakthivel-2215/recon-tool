f9e9a258e7b560b218df9cb81f1bed1f72f49469
Sakthivel-2215 <lsakthivelcyber@gmail.com>
2026-04-29 12:13:32 +0530

    fix port binding

--- backend/server.js
+++ backend/server.js
@@ -193,7 +193,7 @@ app.get('*', (req, res) => {
 
 // ─── SERVER ───────────────────────────────────────
 const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, '0.0.0.0', () => {
+console.log("PORT:", process.env.PORT);
+app.listen(PORT, () => {
   console.log(`🔥 Server running on port ${PORT}`);
 });
